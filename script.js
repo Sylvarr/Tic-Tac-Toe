ee72274c1f61704040f27360b82951de102b1fb1
Whitessx <whitessx@gmail.com>
2023-12-23 22:17:07 +0100

    Add "Play Game" functionality and display winner announcement

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ function gameMatrix() {
     undefined,
   ];
 
+  function startGame() {
+    let playButton = document.querySelector(".play");
+    let matrix = document.querySelector(".matrix");
+    playButton.addEventListener("click", () => {
+      matrix.classList.add("visible");
+    });
+  }
+
+  startGame();
+
   function resetGame() {
     let reset = document.querySelector(".reset");
     reset.addEventListener("click", () => {
@@ -28,48 +38,56 @@ function gameMatrix() {
       array[1] === array[2]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[0]} is the winner!`;
     } else if (
       array[3] !== undefined &&
       array[3] === array[4] &&
       array[4] === array[5]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[3]} is the winner!`;
     } else if (
       array[6] !== undefined &&
       array[6] === array[7] &&
       array[7] === array[8]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[6]} is the winner!`;
     } else if (
       array[0] !== undefined &&
       array[0] === array[3] &&
       array[3] === array[6]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[0]} is the winner!`;
     } else if (
       array[1] !== undefined &&
       array[1] === array[4] &&
       array[4] === array[7]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[1]} is the winner!`;
     } else if (
       array[2] !== undefined &&
       array[2] === array[5] &&
       array[5] === array[8]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[2]} is the winner!`;
     } else if (
       array[0] !== undefined &&
       array[0] === array[4] &&
       array[4] === array[8]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[0]} is the winner!`;
     } else if (
       array[2] !== undefined &&
       array[2] === array[4] &&
       array[4] === array[6]
     ) {
       announcement.classList.add("visible");
+      announcement.innerHTML = `${array[2]} is the winner!`;
     } else {
       return;
     }
@@ -81,6 +99,7 @@ function gameMatrix() {
   function onClickButton(e) {
     let button = e.target;
     if (button.innerHTML !== "") return;
+    if (announcement.classList.contains("visible")) return;
     let symbol = isPlayerTurn ? "X" : "O";
     array[button.value] = button.innerHTML = symbol;
     isPlayerTurn = !isPlayerTurn;
